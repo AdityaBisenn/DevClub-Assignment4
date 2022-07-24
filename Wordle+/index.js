14ee5b985360081fe266d3f792f0b30d3135543b
Aditya Bisen <adityabisen@Adityas-MacBook-Air.local>
2022-07-24 20:16:23 +0530

    Wordle+ added

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -6,11 +6,29 @@ function myFunction(req, res) {
 	// console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
 
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
 
-	res.write(feedback);
-	res.end();
+	const GUESS = req.url.split("?q=")[1]; // Write logic to parse the word which the user guessed from the URL string
+	let feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
+	let secret_arr = SECRET.split("");
+	if (GUESS.length != 5){
+		res.end("Word should be of 5 letters");
+	}
+	else{
+		for(let i=0;i<5;i++){
+			if(GUESS[i]==SECRET[i]){
+				feedback = feedback + "g"
+			}
+			else if (secret_arr.includes(GUESS[i])){
+				feedback = feedback + "y"
+			}
+			else{
+				feedback = feedback + "b"
+			}
+		}
+	}
+
+	
+	res.end(feedback);
 }
 
 http.createServer(myFunction).listen(8080);
