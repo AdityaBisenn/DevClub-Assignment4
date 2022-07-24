0c84949e3a21bf416638819599f5c38e3d173be5
Aditya Bisen <adityabisen@Adityas-MacBook-Air.local>
2022-07-24 11:28:49 +0530

    Moodle+ done

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,27 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector("#login"); // Fill the selector for the login element in ""
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let login_text_arr = login_text.split("\n")
+
+let question = login_text_arr[3]; // Use split and array operations on the login_text string to extract the question
+let question_arr = question.split(" ")
 
 let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+if(question_arr[2]=="second"){
+    answer=question_arr[6];
+}
+else if(question_arr[2]=="first"){
+    answer = question_arr[4];
+}
+else if(question_arr[1]=="subtract"){
+    answer = parseInt(question_arr[2]) - parseInt(question_arr[4]);
+}
+else if(question_arr[1]=="add"){
+    answer = parseInt(question_arr[2]) + parseInt(question_arr[4]);
+}
+
+
+const captcha_input_element = document.querySelector("#valuepkg3"); // Fill the selector for the captcha input element in ""
 captcha_input_element.value = answer;
